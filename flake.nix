c8c3d8fa8aa45784ece71fb613b19a5e6fb12426
Matthias Scholz <MatthiasScholz@users.noreply.github.com>
2023-01-06 20:27:07 +0000

    :bug: missmatching versions

--- flake.nix
+++ flake.nix
@@ -2,8 +2,13 @@
   description = "MacOS basics";
 
   inputs = {
+      # stable
       nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-22.05-darwin";
-      home-manager.url = "github:nix-community/home-manager";
+      home-manager.url = "github:nix-community/home-manager/release-22.05";
+      # most recent
+      #nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+      #home-manager.url = "github:nix-community/home-manager";
+      
       home-manager.inputs.nixpkgs.follows = "nixpkgs";
       # nix will normally use the nixpkgs defined in home-managers inputs, we only want one copy of nixpkgs though
       darwin.url = "github:lnl7/nix-darwin";
@@ -16,10 +21,7 @@
     darwinConfigurations."MacPro" = darwin.lib.darwinSystem {
     # you can have multiple darwinConfigurations per flake, one per hostname
       system = "x86_64-darwin";
-      modules = [
-        home-manager.darwinModules.home-manager
-        ./hosts/MacPro/default.nix
-      ]; # will be important later
+      modules = [ home-manager.darwinModules.home-manager ./hosts/MacPro/default.nix]; # will be important later
     };
   };
 }
